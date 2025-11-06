53009b7367954728523e9f23152b9f898a02df95
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2025-11-06 14:22:22 +0800

    Delete tww/AllClass 11.2.0 directory

--- tww/AllClass 11.2.0/party/OnUpdate.lua
+++
@@ -1,7 +0,0 @@
-local e = aura_env
-if e.initialization then
-    e.updateUnitRange()     -- 更新单位距离
-    e.updateCooldown()      -- 更新技能冷却
-    e.updatePartyValidity() -- 队友是否有效
-    e.updatePartyDamage()   -- 更新队伍伤害
-end
--- tww/AllClass 11.2.0/插入技能/event2.lua
+++
@@ -1,5 +0,0 @@
-function WAdeletethis(event)
-    C_Timer.After(0.5, function()
-        aura_env.ReadKeybindings()
-    end)
-end
