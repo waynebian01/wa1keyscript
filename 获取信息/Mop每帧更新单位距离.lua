e05de249ec92b88b75d05e42e72df33e1aac4e23
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2025-11-12 19:50:33 +0800

    Refactor range checking for Skippy group and nameplates

--- 获取信息/Mop每帧更新单位距离.lua
+++ 获取信息/Mop每帧更新单位距离.lua
@@ -1,4 +1,13 @@
-if not Skippy and not Skippy.Group then return end
-for unit, data in pairs(Skippy.Group) do
-    data.inRange = UnitInRange(unit)
+if not Skippy then return end
+if Skippy.Group then
+    for unit, data in pairs(Skippy.Group) do
+        data.inRange = UnitInRange(unit)
+    end
+end
+if Skippy.Nameplate then
+    for unit, data in pairs(Skippy.Nameplate) do
+        local minRange, maxRange = WeakAuras.GetRange(unit)
+        data.minRange = minRange
+        data.maxRange = maxRange
+    end
 end
