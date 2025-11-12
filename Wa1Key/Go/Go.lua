1c79d1c96da9a72a33981087820eb9666d2238ad
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2025-11-12 19:54:55 +0800

    重新在WA插件内编写了Go,性能更优

---
+++ Wa1Key/Go/Go.lua
@@ -0,0 +1,61 @@
+if not Wa1Key or not Wa1Key.Prop then return true end
+
+local e = aura_env
+--local inVehicle = UnitInVehicle("player")                     -- 载具
+local chatFrame = ChatFrame1EditBox:IsVisible() -- 聊天框
+local darkness = UnitCastingInfo("boss1") == "黑暗降临"
+local minRange, maxRange = WeakAuras.GetRange("target")
+local interrupt = Wa1Key.Prop.Interrupt
+if not maxRange then maxRange = 255 end
+
+local range = e.specRangeMap[e.specID] or 8                    -- 默认8码
+local hekili = Hekili_GetRecommendedAbility and Hekili_GetRecommendedAbility("Primary", 1) -- 获取Hekili推荐技能
+local inRange = true
+
+if hekili and hekili > 0 then
+    local spellInfo = C_Spell.GetSpellInfo(hekili)
+    local inSpellRange = C_Spell.IsSpellInRange(hekili, "target")
+    local isHarmful = C_Spell.IsSpellHarmful(hekili)
+    if spellInfo then
+        if spellInfo.maxRange == 0 then
+            inRange = maxRange <= range
+        else
+            if isHarmful and inSpellRange == false then
+                inRange = false
+            end
+        end
+    end
+end
+
+if interrupt == 1 then
+    Wa1Key.Prop.Go = 254
+    return true
+end
+
+if e.mounted or chatFrame or e.dead or e.travel or e.catStealth or darkness then
+    Wa1Key.Prop.Go = 255
+    return true
+end
+
+if e.vanish then -- 消失
+    Wa1Key.Prop.Go = 1
+    return true
+end
+
+if e.validSkills[hekili] then
+    Wa1Key.Prop.Go = 1
+    return true
+end
+
+if e.healingClass[e.specID] and e.inParty then
+    Wa1Key.Prop.Go = 1
+    return true
+end
+
+if e.targetCanAttack and inRange and e.isCombat and not e.targetisdead then
+    Wa1Key.Prop.Go = 1
+    return true
+end
+
+Wa1Key.Prop.Go = 2
+return true
---
+++ Wa1Key/Go/Go_Event.lua
@@ -0,0 +1,37 @@
+-- PLAYER_TARGET_CHANGED,PLAYER_MOUNT_DISPLAY_CHANGED,PLAYER_DEAD,PLAYER_ALIVE,PLAYER_UNGHOST,PLAYER_REGEN_DISABLED,PLAYER_REGEN_ENABLED,PLAYER_TALENT_UPDATE,UPDATE_SHAPESHIFT_FORM,UPDATE_STEALTH,PLAYER_ENTERING_WORLD,GROUP_ROSTER_UPDATE
+
+function Go_Event(event, arg1)
+    local e = aura_env
+    if event == "PLAYER_TARGET_CHANGED" then
+        e.targetCanAttack = UnitCanAttack("player", "target")
+        e.targetisdead = UnitIsDeadOrGhost("target") -- 目标死亡
+    elseif event == "UNIT_FACTION" and arg1 == "target" then
+        e.targetCanAttack = UnitCanAttack("player", "target")
+    elseif event == "PLAYER_MOUNT_DISPLAY_CHANGED" then
+        e.mounted = IsMounted("player")
+    elseif event == "PLAYER_DEAD" then
+        e.dead = UnitIsDeadOrGhost("player")
+    elseif event == "PLAYER_ALIVE" then
+        e.dead = UnitIsDeadOrGhost("player")
+    elseif event == "PLAYER_UNGHOST" then
+        e.dead = UnitIsDeadOrGhost("player")
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        e.isCombat = true
+    elseif event == "PLAYER_REGEN_ENABLED" then
+        e.isCombat = false
+    elseif event == "PLAYER_TALENT_UPDATE" then
+        e.specIndex = C_SpecializationInfo.GetSpecialization()
+        e.specID = C_SpecializationInfo.GetSpecializationInfo(e.specIndex)
+    elseif event == "UPDATE_SHAPESHIFT_FORM" then
+        e.shapeshiftFormID = GetShapeshiftFormID() -- 变形形态
+        e.travel = e.travelNumber[e.shapeshiftFormID]
+    elseif event == "UPDATE_STEALTH" then
+        e.stealth = C_UnitAuras.GetPlayerAuraBySpellID(5215)
+        e.vanish = C_UnitAuras.GetPlayerAuraBySpellID(11327)
+        e.catStealth = e.shapeshiftFormID == 1 and e.stealth
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        e.Go_Init()
+    elseif event == "GROUP_ROSTER_UPDATE" then
+        e.inParty = UnitPlayerOrPetInParty("player")
+    end
+end
