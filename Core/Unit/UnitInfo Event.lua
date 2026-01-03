de1f529039f3853a063a116ad493cee53a3281d3
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2026-01-03 19:34:19 +0800

    Refactor Unitinfo_event function for clarity

--- Core/Unit/UnitInfo Event.lua
+++ Core/Unit/UnitInfo Event.lua
@@ -1,51 +1,42 @@
--- COMBAT_LOG_EVENT_UNFILTERED, UNIT_HEALTH, UNIT_MAXHEALTH, UNIT_HEAL_ABSORB_AMOUNT_CHANGED, UNIT_HEAL_PREDICTION, UNIT_AURA, UNIT_FACTION, PLAYER_TARGET_CHANGED, PLAYER_FOCUS_CHANGED, NAME_PLATE_UNIT_ADDED, NAME_PLATE_UNIT_REMOVED, ENCOUNTER_START, ENCOUNTER_END, UNIT_IN_RANGE_UPDATE, UNIT_SPELLCAST_SENT, UI_ERROR_MESSAGE, PLAYER_ENTERING_WORLD, GROUP_ROSTER_UPDATE
+-- UNIT_HEALTH, UNIT_MAXHEALTH, UNIT_HEAL_ABSORB_AMOUNT_CHANGED, UNIT_HEAL_PREDICTION, UNIT_AURA, UNIT_FACTION, PLAYER_TARGET_CHANGED, PLAYER_FOCUS_CHANGED, NAME_PLATE_UNIT_ADDED, NAME_PLATE_UNIT_REMOVED, ENCOUNTER_START, ENCOUNTER_END, UNIT_IN_RANGE_UPDATE, UNIT_SPELLCAST_SENT, UI_ERROR_MESSAGE, PLAYER_ENTERING_WORLD, GROUP_ROSTER_UPDATE
 
-function Unitinfo_event(event, arg1, arg2, arg3)
-    local e = aura_env
-    if event == "UNIT_HEALTH" and e.All_Units[arg1] then
-        e.UpdateHealth(arg1, "health", UnitHealth)
-    elseif event == "UNIT_MAXHEALTH" and e.All_Units[arg1] then
-        e.UpdateHealth(arg1, "maxHealth", UnitHealthMax)
-    elseif event == "UNIT_HEAL_ABSORB_AMOUNT_CHANGED" and e.All_Units[arg1] then
-        e.UpdateHealth(arg1, "healAbsorbs", UnitGetTotalHealAbsorbs)
-    elseif event == "UNIT_HEAL_PREDICTION" and e.All_Units[arg1] then
-        e.UpdateHealth(arg1, "healPrediction", aura_env.GetIncomingHeals)
-    elseif event == "UNIT_AURA" and e.All_Units[arg1] then
-        e.UpdateAuraIncremental(arg1, arg2)
-    elseif event == "UNIT_FACTION" and e.All_Units[arg1] then
-        e.UpdateFaction(arg1)
+function Unitinfo_event(event, arg1, arg2, arg3)   
+    if event == "UNIT_HEALTH" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateHealth(arg1, "health", UnitHealth)
+    elseif event == "UNIT_MAXHEALTH" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateHealth(arg1, "healthMax", UnitHealthMax)
+    elseif event == "UNIT_HEAL_ABSORB_AMOUNT_CHANGED" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateHealth(arg1, "healAbsorbs", UnitGetTotalHealAbsorbs)
+    elseif event == "UNIT_HEAL_PREDICTION" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateHealth(arg1, "healPrediction", aura_env.GetIncomingHeals)
+    elseif event == "UNIT_AURA" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateAuraIncremental(arg1, arg2)
+    elseif event == "UNIT_FACTION" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateFaction(arg1)
     elseif event == "PLAYER_TARGET_CHANGED" then
-        e.UpdateCoreUnit("target")
+        aura_env.UpdateCoreUnit("target")
     elseif event == "PLAYER_FOCUS_CHANGED" then
-        e.UpdateCoreUnit("focus")
+        aura_env.UpdateCoreUnit("focus")
     elseif event == "NAME_PLATE_UNIT_ADDED" then
-        e.UpdateNameplateUnit(arg1)
+        aura_env.UpdateNameplateUnit(arg1)
     elseif event == "NAME_PLATE_UNIT_REMOVED" then
-        Skippy.Nameplate[arg1] = { exists = false }
+        aura_env.UpdateNameplateUnit(arg1)
     elseif event == "ENCOUNTER_START" then
-        e.UpdateBossUnit()
+        aura_env.InitBossUnit()
     elseif event == "ENCOUNTER_END" then
-        e.UpdateBossUnit()
-    elseif event == "UNIT_IN_RANGE_UPDATE" and e.All_Units[arg1] then
-        if WeakAuras.IsRetail() then e.UpdateInRange(arg1, arg2) end
+        aura_env.InitBossUnit()
+    elseif event == "UNIT_IN_RANGE_UPDATE" and aura_env.GetUnitObj(arg1) then
+        aura_env.UpdateInRange(arg1, arg2)
     elseif event == "UI_ERROR_MESSAGE" and arg2 == "目标不在视野中" then
-        if Skippy.state.CastTargetUnit then
-            local obj = Skippy.Group[Skippy.state.CastTargetUnit]
-            if obj then
-                obj.inSight = false
-                if obj.inSightTimer then
-                    obj.inSightTimer:Cancel()
-                    obj.inSightTimer = nil
-                end
-                obj.inSightTimer = C_Timer.NewTimer(2, function()
-                    obj.inSight = true
-                    obj.inSightTimer = nil
-                end)
-            end
-        end
-    elseif event == "PLAYER_ENTERING_WORLD" or event == "GROUP_ROSTER_UPDATE" then
-        e.InitUnitMapping()
-        e.InitGroupMembers()
-        e.UpdateAllUnits()
+        aura_env.CheckUnitInsight()
+    elseif event == "GROUP_ROSTER_UPDATE" then
+        C_Timer.After(1, function()
+            aura_env.UpdateGroupUnit()
+        end)
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        aura_env.UpdateCoreUnit("target") -- 初始化目标
+        aura_env.UpdateCoreUnit("focus")  -- 初始化焦点
+        aura_env.InitNameplateUnit()      -- 初始化姓名板
+        aura_env.UpdateGroupUnit()        -- 初始化队伍
     end
 end
