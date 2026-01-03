813f5d877629de6056de83fdcd507b9f66202dd1
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2026-01-03 19:34:46 +0800

    Update and rename UnitInfo EventCombatLog.lua to UnitInfo CLEU.lua

---
+++ Core/Unit/UnitInfo CLEU.lua
@@ -0,0 +1,36 @@
+-- CLEU:UNIT_DIED, CLEU:SPELL_MISSED
+function C(event, timestamp, subEvent, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID,
+           destName, destFlags, destRaidFlags, ...)
+    -- 处理单位死亡
+    if subEvent == "UNIT_DIED" then
+        for k, v in pairs(Skippy.Group) do
+            if v.GUID == destGUID then
+                print(k)
+                v.isDead = UnitIsDeadOrGhost(k)
+            end
+        end
+    end
+
+    -- 处理技能免疫
+    if subEvent == "SPELL_MISSED" then
+        if sourceGUID == UnitGUID("player") and destGUID == UnitGUID("target") then
+            -- 在 SPELL_MISSED 中，... 的第 1 个值是 spellId，第 4 个值是 missType
+            local spellId, spellName, spellSchool, missType, isOffHand, amountMissed, critical = ...
+
+            if missType == "IMMUNE" then
+                local immuneTable = Skippy.Units["target"].immuneSpells
+                local isAlreadyKnown = false
+                for i = 1, #immuneTable do
+                    if immuneTable[i] == spellId then
+                        isAlreadyKnown = true
+                        break
+                    end
+                end
+
+                if not isAlreadyKnown then
+                    table.insert(immuneTable, spellId)
+                end
+            end
+        end
+    end
+end
