4eadf772f06895095846c70a1cb81cf076fae515
waynebian01 <150268824+waynebian01@users.noreply.github.com>
2026-01-03 19:33:07 +0800

    Refactor OnUpdate logic for Skippy unit updates

--- Core/Unit/UnitInfo OnUpdate.lua
+++ Core/Unit/UnitInfo OnUpdate.lua
@@ -1,14 +1,4 @@
 if not Skippy then return end
-if Skippy.Group then
-    for unit, data in pairs(Skippy.Group) do
-        data.inRange = UnitInRange(unit)
-        data.canAssist = UnitCanAssist("player", unit)
-    end
-end
-if Skippy.Nameplate then
-    for unit, data in pairs(Skippy.Nameplate) do
-        local minRange, maxRange = WeakAuras.GetRange(unit)
-        data.minRange = minRange
-        data.maxRange = maxRange
-    end
-end
+aura_env.UpdateGroupInfo()
+aura_env.UpdateMaxAndMinRange("target")
+aura_env.UpdateMaxAndMinRange("focus")
